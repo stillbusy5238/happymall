da1545737b28f77e3d09eaf4c68bb74382053c9c
Yuanjie <zhangyuanjie254@gmail.com>
2018-01-18 17:12:21 -0800

    payment

--- src/page/result/index.js
+++ src/page/result/index.js
@@ -6,6 +6,11 @@ require('page/common/nav-simple/index.js');
 $(function(){
   var type = __mm.getUrlParam('type') || 'default',
   $element = $('.' + type + '-success').show();
+  if(type === 'payment'){
+    var orderNumber = __mm.getUrlParam('orderNumber');
+    var $orderNumber = $element.find('.order-number');
+    $orderNumber .attr('href',$orderNumber.attr('href')+orderNumber);
+  }
 
-  
+
 })
